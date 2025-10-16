475f386e830d629765725ef04cb7cbc746499420
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-16 12:35:59 -0600

    Update ship with movement and normalizing the diagonals.

--- 2D/M04.to_space_and_beyond_workbook/lessons/ship.gd
+++ 2D/M04.to_space_and_beyond_workbook/lessons/ship.gd
@@ -1,4 +1,16 @@
 extends Sprite2D
 
+var max_speed := 600.0
+var velocity := Vector2(0, 0)
+
 func _process(delta: float) -> void:
-	pass
+	var direction := Vector2(0, 0)
+	direction.x = Input.get_axis("move_left", "move_right")
+	direction.y = Input.get_axis("move_up", "move_down")
+	
+	if direction.length() > 1.0:
+		direction = direction.normalized()
+	
+	velocity = direction * max_speed
+	position +=  velocity * delta
+	rotation = velocity.angle()
