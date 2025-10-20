c98a2fcbdc80482c49df596961bc46161c0b2499
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-20 14:24:48 -0600

    Updated random_item_placer to only run placement logic if there are less than 6 items.

--- 2D/M05.loot_it_all_workbook/lessons/random_item_placer.gd
+++ 2D/M05.loot_it_all_workbook/lessons/random_item_placer.gd
@@ -10,17 +10,18 @@ func _ready() -> void:
 	get_node("Timer").timeout.connect(_on_timer_timout)
 
 func _on_timer_timout() -> void:
-	var random_item_scene: PackedScene = item_scenes.pick_random()
-	var item_instance := random_item_scene.instantiate()
-	var viewport_size := get_viewport_rect().size
-	var random_position := Vector2(0.0, 0.0)
-	
-	random_position.x = randf_range(0.0, viewport_size.x)
-	random_position.y = randf_range(0.0, viewport_size.y)
-	item_instance.position = random_position
-	item_instance.tree_exited.connect(_on_tree_exited)
-	
 	if item_count < 6:
+		var viewport_size := get_viewport_rect().size
+		var random_position := Vector2(0.0, 0.0)
+		var random_item_scene: PackedScene = item_scenes.pick_random()
+		var item_instance := random_item_scene.instantiate()
+	
+		random_position.x = randf_range(0.0, viewport_size.x)
+		random_position.y = randf_range(0.0, viewport_size.y)
+		item_instance.position = random_position
+		
+		item_instance.tree_exited.connect(_on_tree_exited)
+		
 		item_count += 1
 		add_child(item_instance)
 	
