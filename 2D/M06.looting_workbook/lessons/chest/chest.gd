0af9eb97e11a35a9e62b378f9f3a6ead45e666ad
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-21 09:22:15 -0600

    Add logic to mouse entering and exiting a chest scene to enlarge and shrink the shader outline.

--- 2D/M06.looting_workbook/lessons/chest/chest.gd
+++ 2D/M06.looting_workbook/lessons/chest/chest.gd
@@ -5,9 +5,15 @@ extends Area2D
 func _ready() -> void:
 	mouse_entered.connect(_on_mouse_entered)
 	mouse_exited.connect(_on_mouse_exited)
-	
+	set_outline_thickness(3.0)
+
+func set_outline_thickness(new_thickness: float) -> void:
+	canvas_group.material.set_shader_parameter("line_thickness", new_thickness)
+
 func _on_mouse_entered() -> void:
-	pass
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 3.0, 6.0, 0.08)
 	
 func _on_mouse_exited() -> void:
-	pass
+	var tween := create_tween()
+	tween.tween_method(set_outline_thickness, 6.0, 3.0, 0.08)
