0e240cb4d00ea9d050663d47d8ff7c904d4dc0c0
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-28 12:03:06 -0600

    Create dictionaries to load texture assets for bodies and expressions. Connect to buttons to change textures.

--- 2D/M07.telling_a_story_workbook/lessons/expressions.gd
+++ 2D/M07.telling_a_story_workbook/lessons/expressions.gd
@@ -8,3 +8,28 @@ extends Control
 @onready var button_regular: Button = %ButtonRegular
 @onready var button_sad: Button = %ButtonSad
 @onready var button_happy: Button = %ButtonHappy
+
+
+var bodies_dict := {
+	"sophia": preload("res://assets/sophia.png"),
+	"pink": preload("res://assets/pink.png"),
+}
+
+
+var expressions_dict := {
+	 "happy": preload("res://assets/emotion_happy.png"),
+	 "regular": preload("res://assets/emotion_regular.png"),
+	 "sad": preload("res://assets/emotion_sad.png"),
+}
+
+
+func _ready() -> void:
+	body.texture = bodies_dict["pink"]
+	expression.texture = expressions_dict["happy"]
+	
+	button_sophia.pressed.connect(func() -> void: body.texture = bodies_dict["sophia"])
+	button_pink.pressed.connect(func() -> void: body.texture = bodies_dict["pink"])
+	button_regular.pressed.connect(func() -> void: expression.texture = expressions_dict["regular"])
+	button_sad.pressed.connect(func() -> void: expression.texture = expressions_dict["sad"])
+	button_happy.pressed.connect(func() -> void: expression.texture = expressions_dict["happy"])
+	
