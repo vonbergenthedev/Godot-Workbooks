9944f8918d8e33eeb0adc71b32e1c8b6badfb1d8
vonbergenthedev <vonbergenthedev@gmail.com>
2025-10-29 12:08:11 -0600

    Add voice and alternative for characters to dialogue_items dictionary.

--- 2D/M07.telling_a_story_workbook/lessons/dialogue.gd
+++ 2D/M07.telling_a_story_workbook/lessons/dialogue.gd
@@ -10,39 +10,72 @@ extends Control
 
 var current_dialogue_item_index := 0
 
+var bodies_dict := {
+	"pink": preload("res://assets/pink.png"),
+	"sophia": preload("res://assets/sophia.png"),
+}
+
 var expressions_dict := {
 	 "happy": preload("res://assets/emotion_happy.png"),
 	 "regular": preload("res://assets/emotion_regular.png"),
 	 "sad": preload("res://assets/emotion_sad.png"),
 }
 
+var voices_dict := {
+	"voice_1": preload("res://assets/talking_synth.ogg"),
+	"voice_2": preload("res://assets/talking_synth_alternate.ogg"),
+}
+
+
 var dialogue_items: Array[Dictionary] = [
 	{
 		"expression": expressions_dict["regular"],
-		"text": "I'm learning about Arrays...",
+		"text": "I've been studying arrays and dictionaries lately.",
+		"body": bodies_dict["sophia"],
+		"voice": voices_dict["voice_1"],
 	},
-	
-	{
-		"expression": expressions_dict["sad"],
-		"text": "...and it is a little bit complicated.",
-	},
-	
-	{
-		"expression": expressions_dict["happy"],
-		"text": "Let's see if I got it right: an array is a list of values!",
-	},
-	
 	{
 		"expression": expressions_dict["regular"],
-		"text": "Did I get it right? Did I?",
+		"text": "Oh, nice. How has it been going?",
+		"body": bodies_dict["pink"],
+		"voice": voices_dict["voice_2"],
 	},
-	
+	{
+		"expression": expressions_dict["sad"],
+		"text": "Well... it's a little complicated!",
+		"body": bodies_dict["sophia"],
+		"voice": voices_dict["voice_1"],
+	},
+	{
+		"expression": expressions_dict["sad"],
+		"text": "Oh!",
+		"body": bodies_dict["pink"],
+		"voice": voices_dict["voice_2"],
+	},
+	{
+		"expression": expressions_dict["regular"],
+		"text": "It sure takes time to click at first.",
+		"body": bodies_dict["pink"],
+		"voice": voices_dict["voice_2"],
+	},
 	{
 		"expression": expressions_dict["happy"],
-		"text": "Hehe! Bye bye~!",
-		
+		"text": "If you keep at it, eventually, you'll get the hang of it!",
+		"body": bodies_dict["pink"],
+		"voice": voices_dict["voice_2"],
 	},
-	
+	{
+		"expression": expressions_dict["regular"],
+		"text": "Mhhh... I see. I'll keep at it, then.",
+		"body": bodies_dict["sophia"],
+		"voice": voices_dict["voice_1"],
+	},
+	{
+		"expression": expressions_dict["happy"],
+		"text": "Thanks for the encouragement. Time to LEARN!!!",
+		"body": bodies_dict["sophia"],
+		"voice": voices_dict["voice_1"],
+	},
 ]
 
 
@@ -54,11 +87,13 @@ func _ready() -> void:
 
 func show_text() -> void:
 	rich_text_label.visible_ratio = 0.0
-	rich_text_label.text = dialogue_items[current_dialogue_item_index]["text"]
+	body.texture = dialogue_items[current_dialogue_item_index]["body"]
 	expression.texture = dialogue_items[current_dialogue_item_index]["expression"]
+	rich_text_label.text = dialogue_items[current_dialogue_item_index]["text"]
+	audio_stream_player.stream = dialogue_items[current_dialogue_item_index]["voice"]
 	
 	var tween := create_tween()
-	var text_appearing_duration := 0.5
+	var text_appearing_duration: float = dialogue_items[current_dialogue_item_index]["text"].length()/ 45.0
 	tween.tween_property(rich_text_label, "visible_ratio", 1.0, text_appearing_duration)
 	
 	var sound_max_offset := audio_stream_player.stream.get_length() - text_appearing_duration
