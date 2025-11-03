b8649d75db16bfa6edabaad6ec663e02449db5ab
vonbergenthedev <vonbergenthedev@gmail.com>
2025-11-03 15:57:51 -0800

    Testing Commit after Windows Migration
    Testing Commit after Windows Migration

--- 2D/M09.top_down_movement_workbook/lessons/runner.gd
+++ 2D/M09.top_down_movement_workbook/lessons/runner.gd
@@ -1,8 +1,6 @@
 extends CharacterBody2D
 
-
 var max_speed = 600.0
-
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
