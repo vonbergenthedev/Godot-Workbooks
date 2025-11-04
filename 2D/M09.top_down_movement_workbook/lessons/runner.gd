815cc7f4d705416806e748c7a5b903b24dc3b629
vonbergenthedev <vonbergenthedev@gmail.com>
2025-11-03 16:02:55 -0800

    Last change to test migration.
    Last change to test migration.

--- 2D/M09.top_down_movement_workbook/lessons/runner.gd
+++ 2D/M09.top_down_movement_workbook/lessons/runner.gd
@@ -6,3 +6,5 @@ func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
 	velocity = direction * max_speed
 	move_and_slide()
+
+#Making a change
