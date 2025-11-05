b959956b1a79c8324d2d5306c53be779483fd265
vonbergenthedev <vonbergenthedev@gmail.com>
2025-11-05 10:30:58 -0700

    Remove static character texture; replace character with animated scene, implement animations attached to movement script.

--- 2D/M09.top_down_movement_workbook/lessons/runner.gd
+++ 2D/M09.top_down_movement_workbook/lessons/runner.gd
@@ -1,39 +1,30 @@
 extends CharacterBody2D
 
-const RUNNER_DOWN = preload("res://assets/runner_down.png")
-const RUNNER_DOWN_RIGHT = preload("res://assets/runner_down_right.png")
-const RUNNER_RIGHT = preload("res://assets/runner_right.png")
-const RUNNER_UP = preload("res://assets/runner_up.png")
-const RUNNER_UP_RIGHT = preload("res://assets/runner_up_right.png")
 
-const UP_LEFT = Vector2.UP + Vector2.LEFT
-const UP_RIGHT = Vector2.UP + Vector2.RIGHT
-const DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
-const DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+@export var max_speed := 600.0
+@export var acceleration := 1200.0
+@export var deceleration := 1080.0
 
-var max_speed = 600.0
-
-@onready var _skin: Sprite2D = $Skin
+@onready var _runner_visual_red: RunnerVisual = %RunnerVisualRed
 
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var direction_discrete := direction.sign()
+	var has_input_direction := direction.length() > 0.0
 	
-	match direction_discrete:
-		Vector2.DOWN:
-			_skin.texture = RUNNER_DOWN
-		Vector2.UP:
-			_skin.texture = RUNNER_UP		
-		Vector2.RIGHT, Vector2.LEFT:
-			_skin.texture = RUNNER_RIGHT
-		UP_RIGHT, UP_LEFT:
-			_skin.texture = RUNNER_UP_RIGHT
-		DOWN_RIGHT, DOWN_LEFT:
-			_skin.texture = RUNNER_DOWN_RIGHT
+	if has_input_direction:
+		var desired_velocity := direction * max_speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
+	
+	if direction.length() > 0.0:
+		_runner_visual_red.angle = rotate_toward(_runner_visual_red.angle, direction.orthogonal().angle(), 8.0 * delta)
+		var current_speed_percent := velocity.length() / max_speed
+		_runner_visual_red.animation_name = (RunnerVisual.Animations.WALK if current_speed_percent < 0.8 else RunnerVisual.Animations.RUN)
 			
-	if direction_discrete.length() > 0:
-		_skin.flip_h = direction.x < 0.0
-		
-	velocity = direction * max_speed
+	else:
+		if velocity.length() < 20.0:
+			_runner_visual_red.animation_name = RunnerVisual.Animations.IDLE
+	
 	move_and_slide()
