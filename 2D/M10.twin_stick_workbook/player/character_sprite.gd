88b03a050b3a3fd17529411d6e8431dba793b9a8
vonbergenthedev <vonbergenthedev@gmail.com>
2025-11-20 09:00:47 -0700

    Add script to character sprite for logic to change sprite texture based on player input direction; player movement work.

---
+++ 2D/M10.twin_stick_workbook/player/character_sprite.gd
@@ -0,0 +1,38 @@
+extends Sprite2D
+
+const GODOT_BOTTOM := preload("res://player/godot_bottom.png")
+const GODOT_BOTTOM_RIGHT := preload("res://player/godot_bottom_right.png")
+const GODOT_RIGHT := preload("res://player/godot_right.png")
+const GODOT_UP := preload("res://player/godot_up.png")
+const GODOT_UP_RIGHT := preload("res://player/godot_up_right.png")
+const VECTOR_UP_RIGHT = Vector2.UP + Vector2.RIGHT
+const VECTOR_DOWN_RIGHT = Vector2.DOWN + Vector2.RIGHT
+const VECTOR_UP_LEFT = Vector2.UP + Vector2.LEFT
+const VECTOR_DOWN_LEFT = Vector2.DOWN + Vector2.LEFT
+
+func _ready() -> void:
+	texture =  GODOT_BOTTOM
+
+func _process(_delta: float) -> void:
+	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
+	var discrete_sign_direction := direction.sign()
+	
+	match discrete_sign_direction:
+		
+		Vector2.DOWN:
+			texture = GODOT_BOTTOM
+
+		Vector2.UP:
+			texture = GODOT_UP
+
+		Vector2.RIGHT, Vector2.LEFT:
+			texture = GODOT_RIGHT
+
+		VECTOR_UP_RIGHT, VECTOR_UP_LEFT:
+			texture = GODOT_UP_RIGHT
+			
+		VECTOR_DOWN_RIGHT, VECTOR_DOWN_LEFT:
+			texture = GODOT_BOTTOM_RIGHT
+			
+	if discrete_sign_direction.length() > 0:
+		flip_h = discrete_sign_direction.x < 0.0
--- 2D/M10.twin_stick_workbook/player/player.gd
+++ 2D/M10.twin_stick_workbook/player/player.gd
@@ -1,14 +1,19 @@
 extends CharacterBody2D
 
-@export var max_speed = 2000.0
+@export var max_speed := 2000.0
+@export var acceleration := 6000.0
+@export var deceleration := 5000.0
 
 var turning_factor := 20.0
 
 func _physics_process(delta: float) -> void:
 	var direction := Input.get_vector("move_left", "move_right", "move_up", "move_down")
-	var desired_velocity: Vector2 = direction * max_speed
-	var steering_factor: Vector2 = desired_velocity - velocity
+	var has_input := direction.length() > 0.0
 	
-	velocity += steering_factor * turning_factor * delta
+	if has_input:
+		var desired_velocity := direction * max_speed
+		velocity = velocity.move_toward(desired_velocity, acceleration * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, deceleration * delta)
 
 	move_and_slide()
