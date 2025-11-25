0d7d364361b0d65e3d25998dffd76a693f2a8fde
vonbergenthedev <vonbergenthedev@gmail.com>
2025-11-25 11:07:36 -0700

    Add logic to handle control of character and weapon using keyboard or gamepad; update hands z_index to display hands in front or behind character sprite.

--- 2D/M10.twin_stick_workbook/player/weapon_pivot.gd
+++ 2D/M10.twin_stick_workbook/player/weapon_pivot.gd
@@ -1,7 +1,27 @@
 extends Node2D
 
+var _is_joypad := false
+var aim_direction: Vector2 = Vector2.ZERO
+
 
 func _process(_delta: float) -> void:
-	var direction_to_pivot := global_position.direction_to(get_global_mouse_position())
-	
-	rotation = direction_to_pivot.angle()
+	if _is_joypad:
+		aim_direction = Input.get_vector("aim_left", "aim_right", "aim_up", "aim_down")
+		
+		if aim_direction.length() > 0.0:
+			rotation = aim_direction.angle()
+			
+	else:
+		aim_direction = global_position.direction_to(get_global_mouse_position())
+		rotation = aim_direction.angle()
+		
+	z_index = 3
+	if aim_direction.y < 0.0:
+		z_index = 1
+
+
+func _unhandled_input(event: InputEvent) -> void:
+	if event is InputEventJoypadMotion:
+		_is_joypad = true
+	if event is InputEventKey:
+		_is_joypad = false
