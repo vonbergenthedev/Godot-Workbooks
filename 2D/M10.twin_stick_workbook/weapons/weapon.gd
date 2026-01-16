ff00bb2f7921457b80ee4259ce8e01798b99cf9a
Ashton VonBergen <vonbergenthedev@gmail.com>
2026-01-16 12:14:54 -0700

    Add error check for bullet scene not being set in the Inspector.

--- 2D/M10.twin_stick_workbook/weapons/weapon.gd
+++ 2D/M10.twin_stick_workbook/weapons/weapon.gd
@@ -9,18 +9,23 @@ extends Node2D
 ## Angle for deflection of bullets (spread) when firing weapon
 @export_range(0, 360, 1, "radians_as_degrees") var random_bullet_deflection:float
 
+
 func _physics_process(_delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 
 ## Function to fire the weapon once
 func shoot() -> void:
+	# Add error check to inform if scene has been left empty in the inspector
+	if bullet_scene == null:
+		push_error("Bullet scene in inspector is null, scene required for weapon to function.")
+		
 	var bullet = bullet_scene.instantiate()
 	
 	bullet.global_position = global_position
 	bullet.global_rotation = global_rotation
 	bullet.max_range = max_range
 	bullet.speed = max_bullet_speed
-	bullet.rotation =+ + randf_range(-random_bullet_deflection, random_bullet_deflection)
+	bullet.rotation =+ + randf_range(-random_bullet_deflection / 2.0, random_bullet_deflection / 2.0)
 	
 	get_tree().current_scene.add_child(bullet)
